bda284b49d41b5832ec331d6ae374504e5e1fa8d
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-01 23:21:52 -0500

    Insertar registros en una tabla

---
+++ tablas/insertar.sql
@@ -0,0 +1,13 @@
+-- Insertar registro en la tabla "Users"
+INSERT INTO
+    Users (name)
+VALUES
+    ('Andres') 
+    
+-- Insertar multiples registros "Users"
+INSERT INTO
+    Users (name, age, city)
+VALUES
+    ('Mayer', 23, 'Bogota'),
+    ('Luis', 15, 'Cali'),
+    ('Maria', 15, 'Tolima');
