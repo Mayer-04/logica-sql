04d65585e040992e665009039ecc32686ca08a57
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-03 10:43:15 -0500

    Mejorando código tablas-relaciones

--- tablas/relaciones.sql
+++ tablas/relaciones.sql
@@ -1,13 +1,59 @@
--- Relaciones entre tablas
-CREATE TABLE Users(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    number int NOT NULL,
-    user_id int,
-    FOREIGN KEY(user_id) REFERENCES users(user_id)
+-- * Relación entre tablas 1:1 (uno a uno)
+-- Tabla 1 
+CREATE TABLE personas (
+    id_persona INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    direccion VARCHAR(100)
 );
 
--- Relaciones alterando la tabla
-ALTER TABLE
-    Users
-ADD
-    CONSTRAINT fk_country_code FOREIGN KEY (countrycode) REFERENCES country (code);
+-- Tabla 2
+CREATE TABLE pasaporte (
+    id_pasaporte INT PRIMARY KEY,
+    numero_pasaporte VARCHAR(20) NOT NULL,
+    fecha_expedicion DATE NOT NULL,
+    fecha_vencimiento DATE NOT NULL,
+    -- Relación por clave unica en la tabla pasaporte con la tabla "personas" y el campo "id_persona"
+    id_persona INT UNIQUE,
+    -- Relación por clave foranea en la tabla pasaporte con la tabla "personas" y el campo "id_persona"
+    FOREIGN KEY (id_persona) REFERENCES personas(id_persona)
+);
+
+-- * Relación entre tablas 1:N (uno a muchos)
+-- Tabla 1
+CREATE TABLE clientes (
+    id_cliente INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL
+);
+
+-- Tabla 2
+CREATE TABLE pedidos (
+    id_pedido INT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    id_cliente INT,
+    -- Relación por clave foranea en la tabla pedidos con la tabla "clientes" y el campo "id_cliente" 
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+-- * Relación entre tablas N:N (muchos a muchos)
+-- Tabla 1
+CREATE TABLE estudiantes (
+    id_estudiante SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+-- Tabla 2
+CREATE TABLE cursos (
+    id_curso SERIAL PRIMARY KEY,
+    nombre_curso VARCHAR(50) NOT NULL
+);
+
+-- Tabla intermedia para la relación muchos a muchos
+CREATE TABLE inscripciones (
+    id_inscripcion SERIAL PRIMARY KEY,
+    id_estudiante INT,
+    id_curso INT,
+    FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id_estudiante),
+    FOREIGN KEY (id_curso) REFERENCES cursos(id_curso)
+);
